04155a9b03554501e5ebfb21d427deaf1f6eca1f
Melissa Real <melissa.real@hotmail.com>
2018-12-30 13:11:49 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -10,7 +10,6 @@ class StudentsController < ApplicationController
   end
 
   def activate
-    binding.pry
     @student = Student.find(params[:id])
     @student.update(if @student.active == true ? false : true)
     redirect_to student_path(@student)
